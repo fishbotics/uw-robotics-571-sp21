36277dc792f4ca07d0dfa587a650bf3e8ac42d5f
Junha Roh <rohjunha@uw.edu>
2021-04-01 17:19:03 -0700

    Remove unused class definitions from __init__.py files

--- nn/optimizers/__init__.py
+++ nn/optimizers/__init__.py
@@ -1,5 +1,4 @@
 from .base_optimizer import BaseOptimizer
 from .sgd_optimizer import SGDOptimizer
-from .momentum_sgd_optimizer import MomentumSGDOptimizer
 
-__all__ = ["BaseOptimizer", "SGDOptimizer", "MomentumSGDOptimizer"]
+__all__ = ["BaseOptimizer", "SGDOptimizer"]
