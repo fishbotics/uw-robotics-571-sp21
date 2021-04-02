48bde2302f871199cbc82a2467084857253c4eff
Junha Roh <rohjunha@uw.edu>
2021-04-02 02:58:21 -0700

    Remove numba implementation

--- nn/layers/relu_layer.py
+++ nn/layers/relu_layer.py
@@ -1,6 +1,3 @@
-import numpy as np
-from numba import njit, prange
-
 from .layer import Layer
 
 
@@ -14,33 +11,4 @@ class ReLULayer(Layer):
 
     def backward(self, previous_partial_gradient):
         # TODO
-        return None
-
-
-class ReLUNumbaLayer(Layer):
-    def __init__(self, parent=None):
-        super(ReLUNumbaLayer, self).__init__(parent)
-        self.data = None
-
-    @staticmethod
-    @njit(parallel=True, cache=True)
-    def forward_numba(data):
-        # TODO Helper function for computing ReLU
-        pass
-
-    def forward(self, data):
-        # TODO
-        self.data = data
-        output = self.forward_numba(data)
-        return output
-
-    @staticmethod
-    @njit(parallel=True, cache=True)
-    def backward_numba(data, grad):
-        # TODO Helper function for computing ReLU gradients
-        pass
-
-    def backward(self, previous_partial_gradient):
-        # TODO
-        self.backward_numba(self.data, previous_partial_gradient)
         return None
