dc6fde238f89a1730e2a8f6781901b769c55321e
Junha Roh <rohjunha@uw.edu>
2021-04-02 02:57:17 -0700

    Modify the submit script

--- hw1/submit.sh
+++ hw1/submit.sh
@@ -12,6 +12,6 @@ prepare ../nn/layers/linear_layer.py
 prepare ../nn/layers/relu_layer.py
 prepare ../nn/optimizers/sgd_optimizer.py
 
-tar cvzf submit.tar.gz submit
+zip -r submit.zip submit
 rm -rf submit/
-echo "Done. Please upload submit.tar.gz to Canvas."
+echo "Done. Please upload submit.zip to Gradescope."
